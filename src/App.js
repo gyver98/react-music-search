ddd1d2622c7b8f6716a3af2d8b8f74fec693d0c6
gyver98 <matthewchoi7@gmail.com>
2017-01-18 11:49:57 +1100

    AlbumList component and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import SearchBar from './component/SearchBar'
+import SearchBar from './component/SearchBar/SearchBar';
+import AlbumList from './component/AlbumList/AlbumList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <AlbumList />
+      </div>
     );
   }
 }
---
+++ src/component/AlbumList/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AlbumList.css';
+
+const AlbumList = (props) => {
+  return (
+    <ul>
+      <li>Album1</li>
+      <li>Album2</li>
+      <li>Album3</li>
+    </ul>
+  )
+}
+
+export default AlbumList;
